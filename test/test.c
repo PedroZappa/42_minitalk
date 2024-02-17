f545fdd2494fcf38779d98ad894a7dd9930401ad
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-02-17 21:35:46 +0000

    signal test : using sigaction

--- test/test.c
+++ test/test.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 18:48:38 by passunca          #+#    #+#             */
-/*   Updated: 2024/02/17 18:58:01 by passunca         ###   ########.fr       */
+/*   Updated: 2024/02/17 21:35:27 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,29 @@ void	ft_handler(int signum);
 int	main(void)
 {
 	/*	Handle a signal	*/
-	signal(SIGINT, ft_handler);
-	while (1)
-	{
-		printf("Yo Whirl!\n");
-		sleep(1);
-	}
+	// signal(SIGINT, ft_handler);
+	// while (1)
+	// {
+	// 	printf("Yo Whirl!\n");
+	// 	sleep(1);
+	// }
 
 	/*	Ignore a signal */
 	// signal(SIGINT, SIG_IGN);
 	// while (1)
 	// 	printf("Yo Whirl!\n");
+	
+	/*	Handle a signal with sigaction() */
+	struct sigaction sa;
+	sa.sa_handler = ft_handler;
+	sa.sa_flags = 0;
+
+	sigaction(SIGINT, &sa, NULL);
+	while (1)
+	{
+		printf("Yo Whirl!\n");
+		sleep(1);
+	}
 	return (0);
 }
 
