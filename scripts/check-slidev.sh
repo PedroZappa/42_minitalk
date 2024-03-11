209abbcec706fcb3b96c2c09e2ede905c807f526
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-03-11 11:17:12 +0000

    scripts/check-slidev.sh : updated

--- scripts/check-slidev.sh
+++ scripts/check-slidev.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 if [ $(! npm list -g | grep slidev/cli | wc -l) -eq 1 ]; then
-	echo "* Slidev is installed globally" && 
+	echo "* Slidev is installed Globally" && 
+	echo "* Starting Slidev" && 
 	slidev;
 else
-	echo "* Building Slidev locally" &&
+	echo "* Building Slidev Locally" &&
 	npm init slidev@latest;
 fi
 
